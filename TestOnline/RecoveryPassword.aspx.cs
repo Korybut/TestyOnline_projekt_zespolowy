c8162c3e17e9159d6528adeca0c93444bf68aa6c
Michał <michal0077@hotmail.com>
2018-04-24 22:06:47 +0200

    okkk

--- TestOnline/RecoveryPassword.aspx.cs
+++ TestOnline/RecoveryPassword.aspx.cs
@@ -18,31 +18,32 @@ namespace TestOnline
 
         }
 
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
-        con.Open();
-        string query = "SELECT count(*) FROM USERS WHERE email='" + TextBox_email.Text + "'";
-
-        SqlCommand cmd = new SqlCommand(query, con);
-        string output = cmd.ExecuteScalar().ToString();
-
-        if (output == "1")
+        protected void Button1_Click(object sender, EventArgs e)
         {
-            var message = new MailMessage();
-            message.From = new MailAddress("inf.koszalin2015@gmail.com", "tesciki.pl");
-            message.To.Add(new MailAddress(TextBox_email.Text));
-            message.Subject = "Test";
-            message.Body = "Treść maila";
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+            con.Open();
+            string query = "SELECT count(*) FROM USERS WHERE email='" + TextBox_email.Text + "'";
 
-            var smtp = new SmtpClient("smtp.gmail.com");
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential("inf.koszalin2015@gmail.com", "coo#2016");
-            smtp.EnableSsl = true;
-            smtp.Port = 587;
-            smtp.Send(message);
+            SqlCommand cmd = new SqlCommand(query, con);
+            string output = cmd.ExecuteScalar().ToString();
 
-            Response.Redirect("~/");
+            if (output == "1")
+            {
+                var message = new MailMessage();
+                message.From = new MailAddress("inf.koszalin2015@gmail.com", "tesciki.pl");
+                message.To.Add(new MailAddress(TextBox_email.Text));
+                message.Subject = "Test";
+                message.Body = "Treść maila";
+
+                var smtp = new SmtpClient("smtp.gmail.com");
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("inf.koszalin2015@gmail.com", "coo#2016");
+                smtp.EnableSsl = true;
+                smtp.Port = 587;
+                smtp.Send(message);
+
+                Response.Redirect("~/");
+            }
         }
     }
 }
