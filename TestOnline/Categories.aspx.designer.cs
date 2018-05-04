418ace36ea695b06aff314462f42fadfaa8bae9f
Michał <michal0077@hotmail.com>
2018-05-04 21:53:40 +0200

    wczytywanie kategorii

--- TestOnline/Categories.aspx.designer.cs
+++ TestOnline/Categories.aspx.designer.cs
@@ -13,57 +13,12 @@ namespace TestOnline {
     public partial class Categories {
         
         /// <summary>
-        /// Kontrolka Label1.
+        /// Kontrolka Repeater1.
         /// </summary>
         /// <remarks>
         /// Pole generowane automatycznie.
         /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
-        /// Kontrolka Label2.
-        /// </summary>
-        /// <remarks>
-        /// Pole generowane automatycznie.
-        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
-        
-        /// <summary>
-        /// Kontrolka Label3.
-        /// </summary>
-        /// <remarks>
-        /// Pole generowane automatycznie.
-        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
-        
-        /// <summary>
-        /// Kontrolka Label4.
-        /// </summary>
-        /// <remarks>
-        /// Pole generowane automatycznie.
-        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label4;
-        
-        /// <summary>
-        /// Kontrolka Label5.
-        /// </summary>
-        /// <remarks>
-        /// Pole generowane automatycznie.
-        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label5;
-        
-        /// <summary>
-        /// Kontrolka Label6.
-        /// </summary>
-        /// <remarks>
-        /// Pole generowane automatycznie.
-        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label6;
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
     }
 }
--- TestOnline/Default.aspx
+++ TestOnline/Default.aspx
@@ -1,7 +1,14 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestOnline.Default" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-    <title>Strona Default - tesciki.pl</title>
+    <title>Strona Główna - tesciki.pl</title>
 </asp:Content>
 <asp:Content ID="ContentPlaceHolder1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="id_kategorii" DataSourceID="SqlDataSource1" ShowHeader="False">
+        <Columns>
+            <asp:BoundField DataField="id_kategorii" HeaderText="id_kategorii" InsertVisible="False" ReadOnly="True" SortExpression="id_kategorii" Visible="False" />
+            <asp:BoundField DataField="nazwa" HeaderText="nazwa" SortExpression="nazwa" />
+        </Columns>
+    </asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:TestyOnlineConnectionString %>" SelectCommand="SELECT [id_kategorii], [nazwa] FROM [KATEGORIE]"></asp:SqlDataSource>
 </asp:Content>
